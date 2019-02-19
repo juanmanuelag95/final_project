8ccede1aa47dba66536a02739bca053529f6e0d4
Juan Alvarez <juanmanuelag95@gmail.com>
2019-02-19 12:34:05 -0600

    user added and runner class

---
+++ src/final_proyect/User.java
@@ -0,0 +1,15 @@
+package final_proyect;
+
+public class User {
+	String mail;
+	String pass;
+	// Which type of user
+	int type;
+	
+	User(){
+		//set to 10 games
+		this.mail = "";
+		this.pass = "";
+		this.type = 0;
+	}
+}
