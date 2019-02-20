2f555151c106b5e8707c95af520775ad1799d091
ManuelOrdoñez <mnu...181192>
2019-02-20 16:35:58 -0600

    stage 6 from cupon complete

---
+++ src/CouponCasses/Case_6.java
@@ -0,0 +1,49 @@
+package CouponCasses;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import final_proyect.Admin;
+import final_proyect.Data;
+
+class DataC extends Data {
+	DataC(){
+		
+		params.put("status", "Enable");
+		params.put("percentage", "10");
+		params.put("max", "2");
+		params.put("startdate", "12/11/2019");
+		params.put("expdate", "12/12/2019");
+		params.put("CuponCode", "TestCode");
+		params.put("Assign_hotel", "hotel");
+		params.put("Assign_Tours", "6 Days Around Thailand");
+		params.put("Assign_Cars", "Kia Pacanto 2014");
+		
+		
+	}
+}
+
+public class Case_6 {
+	
+	public static void main(String[] args) throws InterruptedException {
+	DataC datac = new DataC();
+	Admin admin = new Admin();	
+	
+	WebDriver driver = new ChromeDriver();
+	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	
+	admin.login(driver);
+	admin.generateTicket(driver, datac);
+	
+	if (admin.validateCuponCreated(driver, datac))
+		System.out.println("Cupon has been Created");
+	else
+		System.out.println("Cupon has not been Created");
+	
+	admin.logout(driver);
+	driver.close();
+	
+	}
+
+}
--- src/final_proyect/Admin.java
+++ src/final_proyect/Admin.java
@@ -1,8 +1,10 @@
 package final_proyect;
 	import java.util.List;
 	import org.openqa.selenium.By;
-	import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 	import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class Admin extends User {
 	public Admin() {
@@ -42,4 +44,41 @@ public class Admin extends User {
 		// TODO Auto-generated method stub
 		
 	}
+
+	
+	public void generateTicket (WebDriver driver, Data datac) throws InterruptedException  {
+		
+		driver.findElement(By.xpath("//*[@id=\"social-sidebar-menu\"]/li[7]/a")).click();
+		driver.findElement(By.xpath("//*[@id=\"Hotels\"]/li[1]/a")).click();
+		driver.findElement(By.xpath("//*[@id=\"social-sidebar-menu\"]/li[31]/a")).click();
+		driver.findElement(By.xpath("//*[@id=\"content\"]/div[1]/div[2]/div[1]/button")).click();
+		
+		WebElement Status = driver.findElement(By.name("status"));
+		Select selectStatus = new Select(Status);
+		selectStatus  .selectByVisibleText(datac.params.get("status"));
+		
+		driver.findElement(By.name("rate")).sendKeys(datac.params.get("percentage"));
+		driver.findElement(By.name("max")).sendKeys(datac.params.get("max"));
+		driver.findElement(By.name("startdate")).sendKeys(datac.params.get("startdate"));
+		driver.findElement(By.name("expdate")).sendKeys(datac.params.get("expdate"));
+		driver.findElement(By.xpath("//*[@id=\"addcoupon\"]/div[2]/div[1]/div[2]/div/div[1]/ins")).click();
+		driver.findElement(By.name("code")).sendKeys(datac.params.get("CuponCode"));
+		driver.findElement(By.xpath("//*[@id=\"s2id_autogen2\"]")).sendKeys(datac.params.get("Assign_hotel"));
+		driver.findElement(By.xpath("//*[@id=\"s2id_autogen2\"]")).sendKeys(Keys.ENTER);
+		driver.findElement(By.xpath("//*[@id=\"s2id_autogen4\"]")).sendKeys(datac.params.get("Assign_Tours"));
+		driver.findElement(By.xpath("//*[@id=\"s2id_autogen4\"]")).sendKeys(Keys.ENTER);
+		driver.findElement(By.xpath("//*[@id=\"s2id_autogen6\"]")).sendKeys(datac.params.get("Assign_Cars"));
+		driver.findElement(By.xpath("//*[@id=\"s2id_autogen6\"]")).sendKeys(Keys.ENTER);
+		driver.findElement(By.xpath("//*[@class=\"btn btn-primary submitcoupon\"]")).click();
+		Thread.sleep(6000);
+//		WebElement text = driver.findElement(By.cssSelector("#content > div.panel.panel-default > div.panel-body > div.xcrud > div > div.xcrud-ajax > div.xcrud-list-container > table > tbody > tr:nth-child(0) > td:nth-child(4)"));
+//		String linkTextDear = text.getText();
+//		
+//		System.out.println(linkTextDear);
+	}
+	
+	public boolean validateCuponCreated(WebDriver driver, Data datac) {		
+		 return datac.params.get("CuponCode").contentEquals(driver.findElement(By.cssSelector("div.panel.panel-default > div.panel-body > div.xcrud > div > div.xcrud-ajax > div.xcrud-list-container > table > tbody > tr:nth-child(1) > td:nth-child(4)")).getText()) ? true : false ;
+	}
 }
+
