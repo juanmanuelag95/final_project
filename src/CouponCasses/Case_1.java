7e251f9f2406dbb866a2e88ddc8e3439fbf551e2
ManuelOrdoñez <mnu...181192>
2019-02-22 16:42:16 -0600

    Create Test Case1 Coupon

--- src/CouponCasses/Case_1.java
+++ src/CouponCasses/Case_1.java
@@ -1,5 +1,33 @@
 package CouponCasses;
 
-public class Case_1 {
+import java.text.ParseException;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import final_proyect.Costumer;
+import final_proyect.Data;
+
+class DataC8 extends Data {
+	DataC8() {
+		params.put("", "");
+	}
 }
+
+	public class Case_1 {
+		
+		public static void main(String[] args) throws InterruptedException, ParseException {
+			DataC8 data = new DataC8();
+			WebDriver driver = new ChromeDriver();
+			Costumer cust = new Costumer();
+			
+			cust.login(driver);
+			cust.couponAvailable(driver, data);
+			cust.couponAvailable(driver, data);
+			
+			
+		
+		}
+	}
+	
+	
