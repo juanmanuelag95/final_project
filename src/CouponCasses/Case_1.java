a421b9928de266505abfd1e7566c855008a7256f
ManuelOrdoñez <mnu...181192>
2019-02-25 08:20:49 -0600

    Case 1 Coupons

--- src/CouponCasses/Case_1.java
+++ src/CouponCasses/Case_1.java
@@ -10,7 +10,11 @@ import final_proyect.Data;
 
 class DataC8 extends Data {
 	DataC8() {
-		params.put("", "");
+		params.put("coupon", "Maxlimit");
+		params.put("hotelName", "Ödeme Seçenekleri");
+		params.put("checkin", "22/04/2019");
+		params.put("checkout", "25/04/2019");
+		params.put("people", "1 Adult 1 Child");
 	}
 }
 
@@ -24,8 +28,10 @@ class DataC8 extends Data {
 			cust.login(driver);
 			cust.couponAvailable(driver, data);
 			cust.couponAvailable(driver, data);
-			
-			
+			cust.couponNotAvailable(driver, data);
+			cust.logout(driver);
+			driver.close();
+			System.out.println("Coupone has exceeded all its uses");
 		
 		}
 	}
